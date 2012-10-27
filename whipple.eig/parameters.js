e4abd937f91c9cdebb6f997329b23d8a9dbd59d4
Adrian Cooke <adriangerardcooke@gmail.com>
2012-10-26 17:55:03 -0700

    Wrote parameters test and class.

--- whipple.eig/parameters.js
+++ whipple.eig/parameters.js
@@ -1,26 +1,46 @@
 goog.provide('whipple.eig.Parameters');
+/** @const */
+var pi = 3.141592653589793;
+
 /**
  * @param {!Element} theElements
  * @constructor
  */
 whipple.eig.Parameters = function(thePositionElement){
-    this.theElement = goog.dom.createDom('span',
-    {
-        'class': 'social-popup'
-    });
-    goog.dom.append(/** @type {!Node} */thePositionElement,this.theElement)
-    goog.base(this,this.theElement);
-    this.setHideOnEscape(true);
-    this.setAutoHide(false);
-    this.setPosition(new goog.positioning.AnchoredViewportPosition(
-        thePositionElement,
-        goog.positioning.Corner.TOP_LEFT));
-    //TODO calculate this from size of span and size of popup.
-    var theSpanSize = goog.style.getSize(thePositionElement);
-    var thePopUpSize = goog.style.getSize(this.theElement);
-    //    this.logger_.info('Popupsize:'+String(thePopUpSize))
-    var margin = new goog.math.Box(-thePopUpSize.height-theSpanSize.height/2,
-        0, 0,
-        -thePopUpSize.width/2+theSpanSize.width/2);
-    this.setMargin(margin);
+	this.pi = pi;
+	// Bicycle parameters
+	this.w = 1.02;
+	this.c = 0.08;
+	this.lambda = this.pi / 10.0;
+	this.g = 9.81;
+
+	// Rear Wheel
+	this.rR = 0.3;
+	this.mR = 2.0;
+	this.IRxx = 0.0603;
+	this.IRyy = 0.12;
+
+	// Rear frame & rider
+	this.xB = 0.3;
+	this.zB = -0.9;
+	this.mB = 85.0;
+	this.IBxx = 9.2;
+	this.IByy = 11.0;
+	this.IBzz = 2.8;
+	this.IBxz = 2.4;
+
+	// Fork and handlebars
+	this.xH = 0.9;
+	this.zH = -0.7;
+	this.mH = 4.0;
+	this.IHxx = 0.05892;
+	this.IHyy = 0.06;
+	this.IHzz = 0.00708;
+	this.IHxz = -0.00756;
+
+	// Front wheel
+	this.rF = 0.35;
+	this.mF = 3.0;
+	this.IFxx = 0.1405;
+	this.IFyy = 0.28;
 };
